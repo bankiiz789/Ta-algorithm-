cb1982b2f285966641d87ebbd7da57b6d9db735f
bankiiz789 <sukkarin7303@gmail.com>
2024-05-14 22:36:32 +0700

    remove ans

--- FindDiff/findDiff.js
+++ FindDiff/findDiff.js
@@ -1,10 +1,1 @@
-exports.findTheDifference = function (s, t) {
-  s = s.split("").sort();
-  t = t.split("").sort();
-
-  for (let i = 0; i < t.length; i++) {
-    if (s[i] !== t[i]) return t[i];
-  }
-
-  return "";
-};
+exports.findTheDifference = function (s, t) {};
